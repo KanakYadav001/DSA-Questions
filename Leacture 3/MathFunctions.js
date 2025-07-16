b77813e920c795e10aa82525a084dbc5ead0d14a
Kanak Yadav <yadavgaming8287@gmail.com>
2025-07-17 02:44:18 +0530

    add more math functions

--- Leacture 3/MathFunctions.js
+++ Leacture 3/MathFunctions.js
@@ -2,3 +2,11 @@ console.log(Math.ceil(10.2)); //increase the number at higher level if number in
 console.log(Math.ceil(9));// print  9  because this is not decimal number
 
 console.log(Math.floor(5.9));//decreased the number in points if number is 9.9 it could be 9\
+
+console.log(Math.round(10.5));//Round Off The Number Is .5 the number is going to ceil 
+console.log(Math.round(9.4));//Round Off The Number Is .4 the number is going to floor
+
+console.log(Math.abs(-8)); //return Negitive Value To Positive Value 
+console.log(Math.abs(8)); //Cannot Be change it still Positive
+
+
