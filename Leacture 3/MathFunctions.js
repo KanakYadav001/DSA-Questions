311571385ccd6a9d94da0c54ebfe65304c7e0c26
Kanak Yadav <yadavgaming8287@gmail.com>
2025-07-17 02:29:49 +0530

    first commit

---
+++ Leacture 3/MathFunctions.js
@@ -0,0 +1,4 @@
+console.log(Math.ceil(10.2)); //increase the number at higher level if number in decimal form
+console.log(Math.ceil(9));// print  9  because this is not decimal number
+
+console.log(Math.floor(5.9));//decreased the number in points if number is 9.9 it could be 9\
