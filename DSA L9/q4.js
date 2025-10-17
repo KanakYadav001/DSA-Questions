ad3209080ce1506402db23e6a4c37fde4cb4cf7e
Kanak Yadav <yadavkanak109@gmail.com>
2025-10-17 10:06:24 +0530

    q4 done

---
+++ DSA L9/q4.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")()
+
+let n  = Number(prompt("Enter Rows Number : "))
+
+for(let i=1 ;i<=n ;i++) {
+    for(let j=1 ;j<=n-i ; j++){
+        process.stdout.write(" ")
+    }
+    for(let k =1 ; k<=i ;k++) {
+        process.stdout.write("* ")
+    }
+    console.log()
+}
