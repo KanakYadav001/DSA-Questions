ddb15d94dab032d08b05813019c4783a5e2c2836
Kanak Yadav <yadavkanak109@gmail.com>
2025-10-24 02:44:07 +0530

    q5,6 done day 11 revision

--- DSA L11/q5.js
+++ DSA L11/q5.js
@@ -1,11 +1,19 @@
-let arr = [1,2,3,45,10]
+// let arr = [1,2,3,45,10]
 
-let temp = arr[0]
+// let temp = arr[0]
 
-for(let i = 0;i<arr.length;i++) {
-    arr[i]=arr[i+1]
+// for(let i = 0;i<arr.length;i++) {
+//     arr[i]=arr[i+1]
+// }
+// arr[arr.length-1]= temp
+
+// console.log(arr)
+
+let arr = [20, 30, 40, 50];
+let copy = arr[0];
+for (let i = 0; i < arr.length - 1; i++) {
+  arr[i] = arr[i + 1];
 }
-arr[arr.length-1]= temp
+arr[arr.length - 1] = copy;
 
-
-console.log(arr)
+console.log(arr);
--- DSA L11/q6.js
+++ DSA L11/q6.js
@@ -1,9 +1,19 @@
-let arr =[22,33,44,55,66]
-let temp = arr[arr.length-1]
+// let arr =[22,33,44,55,66]
+// let temp = arr[arr.length-1]
 
-for(let i=arr.length-1;i>0;i--){
-    arr[i]=arr[i-1]
-}
+// for(let i=arr.length-1;i>0;i--){
+//     arr[i]=arr[i-1]
+// }
 
-arr[0]=temp
+// arr[0]=temp
+// console.log(arr)
+
+
+let arr = [10,20,30,40,50]
+copy  = arr[arr.length-1]
+for(let i=arr.length-1 ; i>0 ;i--){
+     arr[i] = arr[i-1]
+} 
+arr[0] = copy
+
 console.log(arr)
