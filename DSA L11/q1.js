078aa519897995123805191d1ddbe58f7bf24073
Kanak Yadav <yadavkanak109@gmail.com>
2025-10-23 22:32:01 +0530

    day 11 revise q1

--- DSA L11/q1.js
+++ DSA L11/q1.js
@@ -1,17 +1,35 @@
-let arr = [22,33,44,55,66,66]
+// let arr = [22,33,44,55,66,66]
+
+// let max = Math.max(arr[0],arr[1])
+// let smax = Math.min(arr[0],arr[1])
+
+// for(let i=2 ;i<arr.length ;i++){
+//     if(max < arr[i]){
+//         smax= max
+//         max = arr[i]
+//     }
+//     else if(arr[i]>smax && arr[i]!=max) {
+//            smax=arr[i]
+//     } 
+// }
+
+// console.log(smax)
+
+
+
+arr=[22,33,44,34,56,89,90,90]
 
 let max = Math.max(arr[0],arr[1])
 let smax = Math.min(arr[0],arr[1])
 
-for(let i=0 ;i<arr.length ;i++){
-    if(max < arr[i]){
-        smax= max
+for(let i = 2;i<arr.length;i++){
+    if(arr[i]>max){
+        smax = max
         max = arr[i]
     }
-    else if(arr[i]>smax && arr[i]!=max) {
-           smax=arr[i]
+    else if(arr[i]>smax && arr[i]!=max){
+        smax = arr[i]
     }
-    
 }
 
-console.log(smax)
+console.log("Second Element is : " + smax)
