85a3b5c458cf9716a3b8686f8ad1ad983b301783
Kanak Yadav <yadavkanak109@gmail.com>
2025-10-25 01:19:48 +0530

    q3 done

---
+++ DSA L13/q3.js
@@ -0,0 +1,12 @@
+let arr  =[2,4,5,6,7,32,1]
+
+for(let i=1 ; i<arr.length;i++ ){
+    let key = arr[i] 
+    j =i-1
+    while(j>=0 && arr[j]>key){
+        arr[j+1] = arr[j]
+        j--
+    }
+    arr[j+1] = key
+}
+console.log(arr)
